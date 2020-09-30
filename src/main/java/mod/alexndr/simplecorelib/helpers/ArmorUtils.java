6e93aecd8d76a93ee84ec66d59028c17b263289d
Sinhika <sinhika@republicofnewhome.org>
2020-09-30 13:35:29 -0500

    useful classes & functions pulled from Netherrocks

---
+++ src/main/java/mod/alexndr/simplecorelib/helpers/ArmorUtils.java
@@ -0,0 +1,37 @@
+package mod.alexndr.simplecorelib.helpers;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ItemStack;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Assorted common utility code
+ *
+ * @author Cadiboo
+ */
+public final class ArmorUtils
+{
+
+    /**
+     * Is player wearing a full set of the same armor material?
+     * @return boolean
+     */
+    public static boolean isPlayerWearingFullSet(PlayerEntity player, @Nonnull IArmorMaterial material)
+    {
+        Iterable<ItemStack> armorList = player.getArmorInventoryList();
+        for (ItemStack stack : armorList)
+        {
+            if (stack.isEmpty()) { return false; }
+            if (! (stack.getItem() instanceof ArmorItem)) { return false; }
+            ArmorItem piece = (ArmorItem) stack.getItem();
+            IArmorMaterial pieceMaterial = piece.getArmorMaterial();
+            if (pieceMaterial != material) {
+                return false;
+            }
+        } // end-for
+        return true;
+    } // end isPlayerWearingFullSet()
+} // end class ModUtil
