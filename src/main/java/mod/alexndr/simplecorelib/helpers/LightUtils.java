85d88c8843382dd59987dfb1d02355577553683e
Sinhika <sinhika@republicofnewhome.org>
2020-09-30 14:16:12 -0500

    added lightvalue functions class

---
+++ src/main/java/mod/alexndr/simplecorelib/helpers/LightUtils.java
@@ -0,0 +1,20 @@
+package mod.alexndr.simplecorelib.helpers;
+
+import java.util.function.ToIntFunction;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.state.BooleanProperty;
+
+public final class LightUtils
+{
+    public static ToIntFunction<BlockState> setFixedLight(int foo)
+    {
+        return (bar) -> { return foo; };
+    }
+    
+    public static ToIntFunction<BlockState> setSwitchedLight(BooleanProperty prop, int foo)
+    {
+        return (bar) -> {return bar.get(prop) ? foo : 0;};
+    }
+   
+} // end class
