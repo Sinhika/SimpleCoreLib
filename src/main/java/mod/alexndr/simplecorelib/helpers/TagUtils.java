fbe7e1fb94df357f08400f81827aec4ab43dacbb
Sinhika <sinhika@republicofnewhome.org>
2020-10-09 14:20:02 -0500

    added a forgeBlockTag() function to TagUtils

--- src/main/java/mod/alexndr/simplecorelib/helpers/TagUtils.java
+++ src/main/java/mod/alexndr/simplecorelib/helpers/TagUtils.java
@@ -1,6 +1,8 @@
 package mod.alexndr.simplecorelib.helpers;
 
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ITag;
 import net.minecraft.tags.ItemTags;
 
@@ -10,4 +12,9 @@ public final class TagUtils
     {
         return ItemTags.makeWrapperTag("forge:" + name);
     }
+    
+    public static ITag.INamedTag<Block> forgeBlockTag(String name) {
+        return BlockTags.makeWrapperTag("forge:" + name);
+    }
+    
 }
