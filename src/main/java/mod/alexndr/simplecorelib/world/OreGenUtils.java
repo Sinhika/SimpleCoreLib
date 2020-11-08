70ac6b1805115ac9c1bed5e436f57bb180842a5d
Sinhika <sinhika@republicofnewhome.org>
2020-11-08 10:05:43 -0600

    ore gen changes

--- src/main/java/mod/alexndr/simplecorelib/world/OreGenUtils.java
+++ src/main/java/mod/alexndr/simplecorelib/world/OreGenUtils.java
@@ -5,26 +5,36 @@ import net.minecraft.block.BlockState;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
-import net.minecraft.world.gen.placement.Placement;
 
 public class OreGenUtils
 {
     public static ConfiguredFeature<?, ?> buildNetherOreFeature(BlockState bstate, ModOreConfig cfg)
     {
         return Feature.ORE.withConfiguration(
-                new OreFeatureConfig(OreFeatureConfig.FillerBlockType.field_241883_b,
-                    bstate, cfg.getVein_size()))
-                .withPlacement(Placement.field_242907_l.configure(cfg.getCfg())
-                        .func_242728_a().func_242731_b(cfg.getVein_count()));
+                new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK,
+                    bstate, cfg.getVein_size())).range(cfg.getCfg().maximum).square().func_242731_b(cfg.getVein_count());
+//                .withPlacement(Placement.field_242907_l.configure(cfg.getCfg())
+//                        .func_242728_a().func_242731_b(cfg.getVein_count()));
+    }
+
+    public static ConfiguredFeature<?, ?> buildNetherRockFeature(BlockState bstate, ModOreConfig cfg)
+    {
+        return Feature.ORE.withConfiguration(
+                new OreFeatureConfig(OreFeatureConfig.FillerBlockType.BASE_STONE_NETHER,
+                    bstate, cfg.getVein_size())).range(cfg.getCfg().maximum).square().func_242731_b(cfg.getVein_count());
+//                .withPlacement(Placement.field_242907_l.configure(cfg.getCfg())
+//                        .func_242728_a().func_242731_b(cfg.getVein_count()));
     }
     
     public static ConfiguredFeature<?, ?> buildOverworldOreFeature(BlockState bstate, ModOreConfig cfg)
     {
-        return Feature.ORE
-                .withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.field_241882_a, 
-                        bstate, cfg.getVein_size()))
-                .withPlacement(Placement.field_242907_l.configure(cfg.getCfg()).func_242728_a()
-                        .func_242731_b(cfg.getVein_count()));
+        return Feature.ORE.withConfiguration(
+                new OreFeatureConfig(OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, 
+                        bstate, cfg.getVein_size())).range(cfg.getCfg().maximum).square().func_242731_b(cfg.getVein_count());
+        /*
+         * .withPlacement(Placement.field_242907_l.configure(cfg.getCfg()).func_242728_a
+         * () .func_242731_b(cfg.getVein_count()));
+         */    
     } // end buildOverworldOreFeature()
     
 } // end class
