2020896de08b889846e391033006ba0bb359afff
Sinhika <sinhika@republicofnewhome.org>
2021-02-28 14:38:41 -0600

    added LootTableInjectorProvider class

--- src/main/java/mod/alexndr/simplecorelib/datagen/LootTableInjectorProvider.java
+++ src/main/java/mod/alexndr/simplecorelib/datagen/LootTableInjectorProvider.java
@@ -1,6 +1,12 @@
 package mod.alexndr.simplecorelib.datagen;
 
 import net.minecraft.data.DataGenerator;
+import net.minecraft.loot.LootParameterSets;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.RandomValueRange;
+import net.minecraft.loot.conditions.RandomChance;
+import net.minecraft.util.ResourceLocation;
 
 public abstract class LootTableInjectorProvider extends AbstractLootTableProvider
 {
@@ -10,5 +16,20 @@ public abstract class LootTableInjectorProvider extends AbstractLootTableProvide
         super(dataGeneratorIn);
         // TODO Auto-generated constructor stub
     }
+    
+    void addInjectionTable(String modid, String table_name, LootPool.Builder pool)
+    {
+        this.addTable(new ResourceLocation(modid, "inject/" + table_name), 
+                LootTable.builder().addLootPool(pool), LootParameterSets.CHEST);
+    }
+    
+    LootPool.Builder createChestPool(int minRolls, int maxRolls, float chanceRoll) 
+    {
+        return LootPool.builder()
+                .name("main")
+                .rolls(RandomValueRange.of((float)minRolls, (float)maxRolls))
+                .acceptCondition(RandomChance.builder(chanceRoll));
+    } // end createChestPool
+    
 
 } // end class
