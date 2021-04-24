4f07af6b93ce5206e7d9d875534ed58a5715f89d
Sinhika <sinhika@republicofnewhome.org>
2021-04-24 13:24:25 -0500

    simplified and reversed how InjectionTableLookup works. Now, if file isn't added to map, it is null, NOT itself

--- src/main/java/mod/alexndr/simplecorelib/helpers/InjectionTableLookup.java
+++ src/main/java/mod/alexndr/simplecorelib/helpers/InjectionTableLookup.java
@@ -4,71 +4,52 @@ import java.util.HashMap;
 
 /**
  * Look up shortcuts for chest loot tables.
+ * Major change in functionality: missing keys return null instead of defaulting to self.
+ * 
  * @author Sinhika
  *
  */
 @SuppressWarnings("serial")
-public class InjectionTableLookup extends HashMap<String, String>
+public abstract class InjectionTableLookup extends HashMap<String, String>
 {
     /**
      * constructor initializes lookup table with chest names that map to abbreviated tables.
-     * Note: assumes 'bastion', 'simple_dungeon', 'abandoned_mineshaft', 'desert_pyramid',
-     *  'jungle_temple', "spawn_bonus_chest", 'underwater_ruin', 'shipwreck', 'nether',
-     *  'stronghold', 'underwater_ruin', and 'village_house' are always provided.
+     * Assume no tables initially, and add in the subclasses, because doing otherwise is a
+     * maintenance nightmare.
      *  
      */
     public InjectionTableLookup()
     {
         super();
+    } // end ctor
+
+    public void AddStandardAliases()
+    {
         this.put("bastion_bridge", "bastion");
         this.put("bastion_hoglin_stable", "bastion");
         this.put("bastion_other", "bastion");
         this.put("bastion_treasure", "bastion");
+        this.put("nether_bridge", "bastion");
+        
         this.put("buried_treasure", "simple_dungeon");
-        this.put("end_city_treasure", null);
-        this.put("igloo_chest", null);
-        this.put("jungle_temple_dispenser", null);
-        this.put("nether_bridge", "nether");
         this.put("pillager_outpost", "simple_dungeon");
-        this.put("ruined_portal", null);
+        this.put("woodland_mansion", "simple_dungeon");
+        
         this.put("shipwreck_map", "shipwreck");
         this.put("shipwreck_supply", "shipwreck");
         this.put("shipwreck_treasure", "shipwreck");
+        
         this.put("stronghold_corridor", "stronghold");
         this.put("stronghold_crossing", "stronghold");
-        this.put("stronghold_library", "stronghold");
+
         this.put("underwater_ruin_big", "underwater_ruin");
         this.put("underwater_ruin_small", "underwater_ruin");
-        this.put("woodland_mansion", "simple_dungeon");
+        
         this.put("village_savanna_house", "village_house");
         this.put("village_plains_house", "village_house");
         this.put("village_desert_house", "village_house");
         this.put("village_snowy_house", "village_house");
         this.put("village_taiga_house", "village_house");
-        this.put("village_armorer", null);
-        this.put("village_butcher", null);
-        this.put("village_cartographer", null);
-        this.put("village_fisher", null);
-        this.put("village_fletcher", null);
-        this.put("village_mason", null);
-        this.put("village_shepherd", null);
-        this.put("village_tannery", null);
-        this.put("village_temple", null);
-        this.put("village_toolsmith", null);
-        this.put("village_weaponsmith", null);
-       
-    } // end ctor
-
-    /**
-     * All unmapped chest names should have key=value. Use getOrDefault() if 
-     * we want a different default.
-     */
-    @Override
-    public String get(Object key)
-    {
-        if (! this.containsKey(key)) {
-            return (String) key;
-        }
-        return super.get(key);
-    }
+    } // end AddStandardAliases()
+    
 } // end class InjectionTableLookup
