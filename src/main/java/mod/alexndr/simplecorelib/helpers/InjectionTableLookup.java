b315f597a7727c00edd0d8d6b5f1d86ef13e9edb
Sinhika <sinhika@republicofnewhome.org>
2021-04-24 15:25:50 -0500

    redid InjectionTableLookup() to add convenience functions for common alias sets

--- src/main/java/mod/alexndr/simplecorelib/helpers/InjectionTableLookup.java
+++ src/main/java/mod/alexndr/simplecorelib/helpers/InjectionTableLookup.java
@@ -23,33 +23,51 @@ public abstract class InjectionTableLookup extends HashMap<String, String>
         super();
     } // end ctor
 
-    public void AddStandardAliases()
+    public void AddAllAliases()
+    {
+        AddNetherAliases();
+        AddOceanAliases();
+        AddVillageHouseAliases();
+        AddStrongholdAliases();
+        AddDungeonAliases();
+    } // end AddAllAliases()
+    
+    public void AddDungeonAliases()
+    {
+        this.put("pillager_outpost", "simple_dungeon");
+        this.put("woodland_mansion", "simple_dungeon");
+    }
+    
+    public void AddStrongholdAliases()
+    {
+        this.put("stronghold_corridor", "stronghold");
+        this.put("stronghold_crossing", "stronghold");
+    }
+    
+    public void AddVillageHouseAliases()
+    {
+        this.put("village_savanna_house", "village_house");
+        this.put("village_plains_house", "village_house");
+        this.put("village_desert_house", "village_house");
+        this.put("village_snowy_house", "village_house");
+        this.put("village_taiga_house", "village_house");
+    }
+    
+    public void AddNetherAliases()
     {
         this.put("bastion_bridge", "bastion");
         this.put("bastion_hoglin_stable", "bastion");
         this.put("bastion_other", "bastion");
         this.put("bastion_treasure", "bastion");
         this.put("nether_bridge", "bastion");
-        
-        this.put("buried_treasure", "simple_dungeon");
-        this.put("pillager_outpost", "simple_dungeon");
-        this.put("woodland_mansion", "simple_dungeon");
-        
+    }
+
+    public void AddOceanAliases()
+    {
         this.put("shipwreck_map", "shipwreck");
         this.put("shipwreck_supply", "shipwreck");
         this.put("shipwreck_treasure", "shipwreck");
-        
-        this.put("stronghold_corridor", "stronghold");
-        this.put("stronghold_crossing", "stronghold");
-
         this.put("underwater_ruin_big", "underwater_ruin");
         this.put("underwater_ruin_small", "underwater_ruin");
-        
-        this.put("village_savanna_house", "village_house");
-        this.put("village_plains_house", "village_house");
-        this.put("village_desert_house", "village_house");
-        this.put("village_snowy_house", "village_house");
-        this.put("village_taiga_house", "village_house");
-    } // end AddStandardAliases()
-    
+    }
 } // end class InjectionTableLookup
