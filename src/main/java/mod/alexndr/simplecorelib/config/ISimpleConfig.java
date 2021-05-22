50c00fbe8053b542567ce9ff4f387b84a1afa840
Sinhika <sinhika@republicofnewhome.org>
2021-05-22 10:41:39 -0500

    possible breaking changes. Made default functions for ISimpleConfig, like should have been originally. Added function isPlayerWearingPartialSet() to ArmorUtils.

--- src/main/java/mod/alexndr/simplecorelib/config/ISimpleConfig.java
+++ src/main/java/mod/alexndr/simplecorelib/config/ISimpleConfig.java
@@ -1,5 +1,8 @@
 package mod.alexndr.simplecorelib.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Provides some necessary functions that our configs will need to respond to 
  * various condition checks.
@@ -7,22 +10,35 @@ package mod.alexndr.simplecorelib.config;
  */
 public interface ISimpleConfig
 {
-    /**
+    // recipe flags
+    public static Map<String, Boolean> flags = new HashMap<>();
+    
+   /**
      * get the value of flag "n".
      * @param n - name of flag
      * @return boolean value.
      */
-    public boolean getFlag(String n);
+    default public boolean getFlag(String n)
+    {
+        Boolean obj = flags.get(n);
+        return obj != null && obj;
+    }
     
     /**
      * clear the flag collection.
      */
-    public void clear();
+    default public void clear()
+    {
+        flags.clear();
+    }
     
     /**
      * add a flag to the flag collection
      * @param n - name of flag
      * @param val - boolean value of flag
      */
-    public void putFlag(String n, boolean val);
+    default public void putFlag(String n, boolean val)
+    {
+        flags.put(n, val);
+    }
 } // end interface
--- src/main/java/mod/alexndr/simplecorelib/helpers/ArmorUtils.java
+++ src/main/java/mod/alexndr/simplecorelib/helpers/ArmorUtils.java
@@ -1,9 +1,12 @@
 package mod.alexndr.simplecorelib.helpers;
 
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.ItemStack;
+
+import java.util.List;
 
 import javax.annotation.Nonnull;
 
@@ -14,6 +17,30 @@ import javax.annotation.Nonnull;
  */
 public final class ArmorUtils
 {
+    /**
+     * Is player wearing a partial set of the same armor material?
+     * 
+     * @param[in] slot_list list of slots required.
+     * @return boolean
+     */
+    public static boolean isPlayerWearingPartialSet(PlayerEntity player, @Nonnull IArmorMaterial material,
+            @Nonnull Iterable<EquipmentSlotType> slot_list)
+    {
+        List<ItemStack> armorList = (List<ItemStack>) player.getArmorSlots();
+        for (EquipmentSlotType slot : slot_list)
+        {
+            ItemStack stack = armorList.get(slot.getIndex());
+            if (stack.isEmpty()) { return false; }
+            if (!(stack.getItem() instanceof ArmorItem)) { return false; }
+            ArmorItem piece = (ArmorItem) stack.getItem();
+            IArmorMaterial pieceMaterial = piece.getMaterial();
+            if (pieceMaterial != material)
+            {
+                return false;
+            }
+        }
+        return true;
+    } // end ()
 
     /**
      * Is player wearing a full set of the same armor material?
