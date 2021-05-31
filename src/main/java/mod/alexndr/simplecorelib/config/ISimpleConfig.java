33a8fc57250a90c024b2d78f557213a91cfc2197
Sinhika <sinhika@republicofnewhome.org>
2021-05-31 15:47:45 -0500

    split ISimpleConfig into interface ISimpleConfig and class SimpleConfig, because implementation details should be in a heritable class

--- src/main/java/mod/alexndr/simplecorelib/config/ISimpleConfig.java
+++ src/main/java/mod/alexndr/simplecorelib/config/ISimpleConfig.java
@@ -1,8 +1,5 @@
 package mod.alexndr.simplecorelib.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Provides some necessary functions that our configs will need to respond to 
  * various condition checks.
@@ -10,35 +7,23 @@ import java.util.Map;
  */
 public interface ISimpleConfig
 {
-    // recipe flags
-    public static Map<String, Boolean> flags = new HashMap<>();
-    
    /**
      * get the value of flag "n".
      * @param n - name of flag
      * @return boolean value.
      */
-    default public boolean getFlag(String n)
-    {
-        Boolean obj = flags.get(n);
-        return obj != null && obj;
-    }
+    public boolean getFlag(String n);
     
     /**
      * clear the flag collection.
      */
-    default public void clear()
-    {
-        flags.clear();
-    }
+    public void clear();
     
     /**
      * add a flag to the flag collection
      * @param n - name of flag
      * @param val - boolean value of flag
      */
-    default public void putFlag(String n, boolean val)
-    {
-        flags.put(n, val);
-    }
+    public void putFlag(String n, boolean val);
+    
 } // end interface
