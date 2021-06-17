5cfa481a0b810c68b69ba3f7506c545f35f08e04
Sinhika <sinhika@republicofnewhome.org>
2021-06-17 11:57:35 -0500

    discovered I had accidentally created two ClientUtils classes. Merged them

--- src/main/java/mod/alexndr/simplecorelib/client/ClientUtils.java
+++ src/main/java/mod/alexndr/simplecorelib/client/ClientUtils.java
@@ -1,11 +1,23 @@
 package mod.alexndr.simplecorelib.client;
 
+import java.util.Random;
+
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemModelsProperties;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
 
-public class ClientUtils
+/**
+ * small functions that are used client-side.
+ * @author Sinhika
+ *
+ */
+public final class ClientUtils
 {
+    /**
+     * register client-side bow model properties so that bow draw ("pull") is properly rendered.
+     * @param bow
+     */
     public static void setupBowModelProperties(Item bow) 
     {
         ItemModelsProperties.register(bow, new ResourceLocation("pull"), (p0, p1, p2) -> {
@@ -22,6 +34,33 @@ public class ClientUtils
         ItemModelsProperties.register(bow, new ResourceLocation("pulling"), (p0, p1, p2) -> {
             return p2 != null && p2.isUsingItem() && p2.getUseItem() == p0 ? 1.0F : 0.0F;
         });
-    }
+    } // end setupBowModelProperties()
+    
+    /**
+     * Standard equation for generating random particle positions for blocks that spew random
+     * particles. Used as a helper function in animateTick().
+     * @param pos as provided by animateTick()
+     * @param rand as provided by animateTick()
+     * @return array with args for World.addParticle position & vector.
+     */
+    public static double[] findBlockParticleVector(BlockPos pos, Random rand)
+    {
+        double pv[] = new double[6];
+        
+        float f1 = (float)pos.getX() - 0.5F;
+        float f2 = (float)pos.getY() - 0.5F;
+        float f3 = (float)pos.getZ() - 0.5F;
+        float f4 = rand.nextFloat() * 2.0f;
+        float f5 = rand.nextFloat() * 2.0f;
+        float f6 = rand.nextFloat() * 2.0f;
+        pv[0] = (double)(f1 + f4);
+        pv[1] = (double)(f2 + f5);
+        pv[2] = (double)(f3 + f6);
+        pv[3] = 0.0D;
+        pv[4] = 0.0D;
+        pv[5] = 0.0D;
+        
+        return pv;
+    } // end findBlockParticleVector()
 
 } // end class
