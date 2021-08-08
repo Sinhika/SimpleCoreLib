5aaaed89d6d2f7431c1ea03fc2beea385c142c30
Sinhika <sinhika@republicofnewhome.org>
2021-08-08 07:49:09 -0500

    start adding datagen

---
+++ src/main/java/mod/alexndr/simplecorelib/datagen/SimpleCoreDataGenerator.java
@@ -0,0 +1,24 @@
+package mod.alexndr.simplecorelib.datagen;
+
+import mod.alexndr.simplecorelib.SimpleCoreLib;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
+
+@EventBusSubscriber(modid=SimpleCoreLib.MODID, bus=EventBusSubscriber.Bus.MOD)
+public class SimpleCoreDataGenerator
+{
+	@SubscribeEvent
+	public static void gatherData(GatherDataEvent event)
+	{
+		DataGenerator gen = event.getGenerator();
+        if (event.includeServer())
+        {
+        }
+        if (event.includeClient())
+        {
+        }
+	} // end gatherData()
+	
+} // end class
