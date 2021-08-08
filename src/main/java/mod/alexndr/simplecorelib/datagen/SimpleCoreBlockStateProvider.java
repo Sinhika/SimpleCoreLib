71d98faeeced48bde74ce1fc0e84f4f07ae0d040
Sinhika <sinhika@republicofnewhome.org>
2021-08-08 09:23:08 -0500

    added datagenned models and blockstate files

--- src/main/java/mod/alexndr/simplecorelib/datagen/SimpleCoreBlockStateProvider.java
+++ src/main/java/mod/alexndr/simplecorelib/datagen/SimpleCoreBlockStateProvider.java
@@ -1,6 +1,7 @@
 package mod.alexndr.simplecorelib.datagen;
 
 import mod.alexndr.simplecorelib.SimpleCoreLib;
+import mod.alexndr.simplecorelib.content.VeryAbstractFurnaceBlock;
 import mod.alexndr.simplecorelib.init.ModBlocks;
 import net.minecraft.core.Direction;
 import net.minecraft.data.DataGenerator;
@@ -24,13 +25,13 @@ public class SimpleCoreBlockStateProvider extends BlockStateProvider
 	{
 		// block models
 		ModelFile testFurnaceModel = this.models().orientable("test_furnace", 
-														 new ResourceLocation(SimpleCoreLib.MODID, "test_furnace_side"),
-														 new ResourceLocation(SimpleCoreLib.MODID, "test_furnace_front"),
-														 new ResourceLocation(SimpleCoreLib.MODID, "test_furnace_top"));
-		ModelFile testFurnaceModel_lit = this.models().orientable("test_furnace_on", 
-				 new ResourceLocation(SimpleCoreLib.MODID, "test_furnace_side"),
-				 new ResourceLocation(SimpleCoreLib.MODID, "test_furnace_front_lit"),
-				 new ResourceLocation(SimpleCoreLib.MODID, "test_furnace_top"));
+														 new ResourceLocation(SimpleCoreLib.MODID, "block/test_furnace_side"),
+														 new ResourceLocation(SimpleCoreLib.MODID, "block/test_furnace_front"),
+														 new ResourceLocation(SimpleCoreLib.MODID, "block/test_furnace_top"));
+		ModelFile testFurnaceModel_lit = this.models().orientable("block/test_furnace_on", 
+				 new ResourceLocation(SimpleCoreLib.MODID, "block/test_furnace_side"),
+				 new ResourceLocation(SimpleCoreLib.MODID, "block/test_furnace_front_lit"),
+				 new ResourceLocation(SimpleCoreLib.MODID, "block/test_furnace_top"));
 		
 		// item models
 		this.itemModels().withExistingParent("test_furnace", new ResourceLocation(SimpleCoreLib.MODID, "block/test_furnace"));
@@ -38,21 +39,21 @@ public class SimpleCoreBlockStateProvider extends BlockStateProvider
 		
 		// blockstates
 		this.getVariantBuilder(ModBlocks.test_furnace.get())
-			.partialState().with(BlockStateProperties.FACING, Direction.NORTH).with(BlockStateProperties.LIT, false)
+			.partialState().with(VeryAbstractFurnaceBlock.FACING, Direction.NORTH).with(BlockStateProperties.LIT, false)
 			.setModels(new ConfiguredModel(testFurnaceModel))
-			.partialState().with(BlockStateProperties.FACING, Direction.SOUTH).with(BlockStateProperties.LIT, false)
+			.partialState().with(VeryAbstractFurnaceBlock.FACING, Direction.SOUTH).with(BlockStateProperties.LIT, false)
 			.setModels(new ConfiguredModel(testFurnaceModel, 0, 180, false))
-			.partialState().with(BlockStateProperties.FACING, Direction.WEST).with(BlockStateProperties.LIT, false)
+			.partialState().with(VeryAbstractFurnaceBlock.FACING, Direction.WEST).with(BlockStateProperties.LIT, false)
 			.setModels(new ConfiguredModel(testFurnaceModel, 0, 270, false))
-			.partialState().with(BlockStateProperties.FACING, Direction.EAST).with(BlockStateProperties.LIT, false)
+			.partialState().with(VeryAbstractFurnaceBlock.FACING, Direction.EAST).with(BlockStateProperties.LIT, false)
 			.setModels(new ConfiguredModel(testFurnaceModel, 0, 90, false))
-			.partialState().with(BlockStateProperties.FACING, Direction.NORTH).with(BlockStateProperties.LIT, true)
+			.partialState().with(VeryAbstractFurnaceBlock.FACING, Direction.NORTH).with(BlockStateProperties.LIT, true)
 			.setModels(new ConfiguredModel(testFurnaceModel_lit))
-			.partialState().with(BlockStateProperties.FACING, Direction.SOUTH).with(BlockStateProperties.LIT, true)
+			.partialState().with(VeryAbstractFurnaceBlock.FACING, Direction.SOUTH).with(BlockStateProperties.LIT, true)
 			.setModels(new ConfiguredModel(testFurnaceModel_lit, 0, 180, false))
-			.partialState().with(BlockStateProperties.FACING, Direction.WEST).with(BlockStateProperties.LIT, true)
+			.partialState().with(VeryAbstractFurnaceBlock.FACING, Direction.WEST).with(BlockStateProperties.LIT, true)
 			.setModels(new ConfiguredModel(testFurnaceModel_lit, 0, 270, false))
-			.partialState().with(BlockStateProperties.FACING, Direction.EAST).with(BlockStateProperties.LIT, true)
+			.partialState().with(VeryAbstractFurnaceBlock.FACING, Direction.EAST).with(BlockStateProperties.LIT, true)
 			.setModels(new ConfiguredModel(testFurnaceModel_lit, 0, 90, false));
 
 	}
