bbebfc665e1ae59ffdd142d9b151aa516a769758
Sinhika <sinhika@republicofnewhome.org>
2021-08-14 14:55:28 -0500

    revised ModOreConfig class for new realities; revised OreGenUtils for 1.17.1

--- src/main/java/mod/alexndr/simplecorelib/config/ModOreConfig.java
+++ src/main/java/mod/alexndr/simplecorelib/config/ModOreConfig.java
@@ -1,25 +1,31 @@
 package mod.alexndr.simplecorelib.config;
 
-import net.minecraft.world.level.levelgen.feature.configurations.RangeDecoratorConfiguration;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
 
 public class ModOreConfig
 {
-    protected RangeDecoratorConfiguration cfg;
+	protected int range_type;
     protected int vein_size;
     protected int vein_count;
+    protected VerticalAnchor bottom, top;
     
-    public ModOreConfig(RangeDecoratorConfiguration cfg, int vein_size, int vein_count)
+    public static final int UNIFORM = 0;
+    public static final int TRIANGLE = 1;
+    
+    public ModOreConfig(int range_type, int vein_size, int vein_count, VerticalAnchor lower, VerticalAnchor upper )
     {
-        this.cfg = cfg;
+        this.range_type = range_type;
         this.vein_size = vein_size;
         this.vein_count = vein_count;
+        this.top = lower;
+        this.bottom = upper;
     }
 
-    public RangeDecoratorConfiguration getCfg()
+    public int getRange_type()
     {
-        return cfg;
+    	return range_type;
     }
-
+    
     public int getVein_size()
     {
         return vein_size;
@@ -28,7 +34,17 @@ public class ModOreConfig
     public int getVein_count()
     {
         return vein_count;
-    } 
-    
+    }
+
+	public VerticalAnchor getBottom()
+	{
+		return bottom;
+	}
+
+	public VerticalAnchor getTop()
+	{
+		return top;
+	}
+
     
 } // end class ModOreConfig
--- src/main/java/mod/alexndr/simplecorelib/world/OreGenUtils.java
+++ src/main/java/mod/alexndr/simplecorelib/world/OreGenUtils.java
@@ -1,44 +1,84 @@
 package mod.alexndr.simplecorelib.world;
 
+import java.util.List;
+
 import mod.alexndr.simplecorelib.config.ModOreConfig;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.Registry;
 import net.minecraft.data.BuiltinRegistries;
+import net.minecraft.data.worldgen.Features;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration.TargetBlockState;
 
 public class OreGenUtils
 {
-    public static ConfiguredFeature<?, ?> buildOverworldOreFeature(
-                                Feature<OreConfiguration> feature, BlockState bstate, ModOreConfig cfg)
-    {
-        return feature.configured(
-                new OreConfiguration(OreConfiguration.Predicates.NATURAL_STONE, bstate, cfg.getVein_size()))
-                .range(cfg.getCfg()).squared().count(cfg.getVein_count());
-    } // end buildOverworldOreFeature()
+//	public static final ImmutableList<OreConfiguration.TargetBlockState> ORE_COPPER_TARGET_LIST = ImmutableList.of(
+//			OreConfiguration.target(OreConfiguration.Predicates.STONE_ORE_REPLACEABLES, Features.States.COPPER_ORE),
+//			OreConfiguration.target(OreConfiguration.Predicates.DEEPSLATE_ORE_REPLACEABLES,
+//					Features.States.DEEPSLATE_COPPER_ORE));
 
+// public static final ConfiguredFeature<?, ?> ORE_COPPER = register("ore_copper",
+//		Feature.ORE.configured(new OreConfiguration(ORE_COPPER_TARGET_LIST, 10))
+//			.rangeTriangle(VerticalAnchor.absolute(0), VerticalAnchor.absolute(96)).squared().count(6));
 
-    public static ConfiguredFeature<?, ?> buildNetherOreFeature(
-                            Feature<OreConfiguration> feature, BlockState bstate, ModOreConfig cfg)
-    {
-        return feature
-                .configured(
-                        new OreConfiguration(OreConfiguration.Predicates.NETHERRACK, bstate, cfg.getVein_size()))
-                .range(cfg.getCfg()).squared().count(cfg.getVein_count());
-    }
+//	public static final ConfiguredFeature<?, ?> ORE_LAPIS = register("ore_lapis",
+//			Feature.ORE.configured(new OreConfiguration(ORE_LAPIS_TARGET_LIST, 7))
+//					.rangeTriangle(VerticalAnchor.absolute(0), VerticalAnchor.absolute(30)).squared());
 
-    public static ConfiguredFeature<?, ?> buildNetherRockFeature(
-                                    Feature<OreConfiguration> feature, BlockState bstate, ModOreConfig cfg)
-    {
-        return feature.configured(
-                    new OreConfiguration(OreConfiguration.Predicates.NETHER_ORE_REPLACEABLES, bstate, cfg.getVein_size()))
-                .range(cfg.getCfg()).squared().count(cfg.getVein_count());
-    }
-    
-    public static void registerFeature(String modid, String name, ConfiguredFeature<?, ?> cfg_feature)
-    {
-        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new ResourceLocation(modid, name), cfg_feature);
-    }
+	public static ConfiguredFeature<?, ?> buildOverworldOreFeature(List<TargetBlockState> target_list, ModOreConfig cfg)
+	{
+		if (cfg.getRange_type() == ModOreConfig.TRIANGLE)
+		{
+			return Feature.ORE.configured(new OreConfiguration(target_list, cfg.getVein_size()))
+					.rangeTriangle(cfg.getBottom(), cfg.getTop()).squared().count(cfg.getVein_count());
+		} else
+		{
+			return Feature.ORE.configured(new OreConfiguration(target_list, cfg.getVein_size()))
+					.rangeUniform(cfg.getBottom(), cfg.getTop()).squared().count(cfg.getVein_count());
+		}
+	} // end buildOverworldOreFeature()
+
+//	public static final ConfiguredFeature<?, ?> ORE_QUARTZ_NETHER = register("ore_quartz_nether", 
+//			Feature.ORE.configured(
+//					new OreConfiguration(OreConfiguration.Predicates.NETHERRACK, Features.States.NETHER_QUARTZ_ORE, 14))
+//			.range(Features.Decorators.RANGE_10_10).squared().count(16));
+
+	public static ConfiguredFeature<?, ?> buildNetherOreFeature(BlockState bstate, ModOreConfig cfg)
+	{
+		return Feature.ORE.configured(new OreConfiguration(OreConfiguration.Predicates.NETHERRACK, bstate, cfg.getVein_size()))
+				.range(Features.Decorators.RANGE_10_10).squared().count(cfg.getVein_count());
+	}
+
+	
+//	public static final ConfiguredFeature<?, ?> ORE_DEBRIS_LARGE = register("ore_debris_large",
+//			Feature.SCATTERED_ORE.configured(new OreConfiguration(OreConfiguration.Predicates.NETHER_ORE_REPLACEABLES,
+//							Features.States.ANCIENT_DEBRIS, 3, 1.0F))
+//					.rangeTriangle(VerticalAnchor.absolute(8), VerticalAnchor.absolute(24)).squared());
+//	public static final ConfiguredFeature<?, ?> ORE_DEBRIS_SMALL = register("ore_debris_small",
+//			Feature.SCATTERED_ORE.configured(new OreConfiguration(OreConfiguration.Predicates.NETHER_ORE_REPLACEABLES,
+//					Features.States.ANCIENT_DEBRIS, 2, 1.0F)).range(Features.Decorators.RANGE_8_8).squared());
+	
+	public static ConfiguredFeature<?, ?> buildNetherRockFeature(BlockState bstate, ModOreConfig cfg)
+	{
+		if (cfg.getRange_type() == ModOreConfig.TRIANGLE)
+		{
+			return Feature.SCATTERED_ORE.configured(
+					new OreConfiguration(OreConfiguration.Predicates.NETHER_ORE_REPLACEABLES, bstate, cfg.getVein_size()))
+						.rangeTriangle(cfg.getBottom(), cfg.getTop()).squared().count(cfg.getVein_count());
+		} 
+		else
+		{
+			return Feature.SCATTERED_ORE.configured(
+					new OreConfiguration(OreConfiguration.Predicates.NETHER_ORE_REPLACEABLES, bstate, cfg.getVein_size()))
+						.range(Features.Decorators.RANGE_8_8).squared().count(cfg.getVein_count());
+		}
+	}
+
+	public static void registerFeature(String modid, String name, ConfiguredFeature<?, ?> cfg_feature)
+	{
+		Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new ResourceLocation(modid, name), cfg_feature);
+	}
 } // end class
