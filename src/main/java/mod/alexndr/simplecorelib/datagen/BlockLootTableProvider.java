5eaca4e8ab164d924a0f2c9d6959f0b129a43ab4
Sinhika <sinhika@republicofnewhome.org>
2021-09-11 12:05:39 -0500

    needed special block loot drop type for slabs

--- src/main/java/mod/alexndr/simplecorelib/datagen/BlockLootTableProvider.java
+++ src/main/java/mod/alexndr/simplecorelib/datagen/BlockLootTableProvider.java
@@ -3,11 +3,14 @@ package mod.alexndr.simplecorelib.datagen;
 import net.minecraft.advancements.critereon.EnchantmentPredicate;
 import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.advancements.critereon.MinMaxBounds;
+import net.minecraft.advancements.critereon.StatePropertiesPredicate;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.block.state.properties.SlabType;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
@@ -18,8 +21,10 @@ import net.minecraft.world.level.storage.loot.functions.ApplyExplosionDecay;
 import net.minecraft.world.level.storage.loot.functions.CopyNameFunction;
 import net.minecraft.world.level.storage.loot.functions.CopyNameFunction.NameSource;
 import net.minecraft.world.level.storage.loot.functions.FunctionUserBuilder;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.predicates.ExplosionCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.MatchTool;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
@@ -50,7 +55,7 @@ public abstract class BlockLootTableProvider extends AbstractLootTableProvider
      */
     protected void standardDropTable(Block b)
     {
-        blockTable(b, LootTable.lootTable().withPool(createStandardDrops(b)));
+         blockTable(b, LootTable.lootTable().withPool(createStandardDrops(b)));
     }
 
     /**
@@ -78,6 +83,17 @@ public abstract class BlockLootTableProvider extends AbstractLootTableProvider
         blockTable(b, LootTable.lootTable().withPool(createItemWithNameCopy(ii)));
     }
 
+    /**
+     * Create a block loot table that drops 1 or 2 slabs depending if the source block is
+     * a single slab or a double slab.
+     * 
+     * @param b slab block being harvested.
+     */
+    protected void slabDropTable(Block b)
+    {
+		blockTable(b, LootTable.lootTable().withPool(createSlabDrops(b)));
+    }
+    
     protected void blockTable(Block b, LootTable.Builder lootTable) 
     {
         addTable(b.getLootTable(), lootTable, LootContextParamSets.BLOCK);
@@ -90,6 +106,16 @@ public abstract class BlockLootTableProvider extends AbstractLootTableProvider
                 .add(LootItem.lootTableItem(itemProvider));
     }
 
+    protected LootPool.Builder createSlabDrops(Block b)
+    {
+    	return LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F))
+				.add(withExplosionDecay(b, LootItem.lootTableItem(b)
+						.apply(SetItemCountFunction.setCount(ConstantValue.exactly(2.0F))
+								.when(LootItemBlockStatePropertyCondition.hasBlockStateProperties(b)
+										.setProperties(StatePropertiesPredicate.Builder.properties()
+												.hasProperty(SlabBlock.TYPE, SlabType.DOUBLE))))));
+    }
+    
     protected LootPool.Builder createItemWithNameCopy(Item itemProvider)
     {
         return LootPool.lootPool().setRolls(ConstantValue.exactly(1))
