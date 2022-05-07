3334ebf992d1a17d8aa2c1b2f6f8b5e2fc5d19bb
Sinhika <sinhika@republicofnewhome.org>
2022-05-07 15:55:55 -0500

    updated json

--- src/main/java/mod/alexndr/simplecorelib/api/client/jei/VeryAbstractFurnaceVariantCategory.java
+++ src/main/java/mod/alexndr/simplecorelib/api/client/jei/VeryAbstractFurnaceVariantCategory.java
@@ -10,6 +10,9 @@ import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import mod.alexndr.simplecorelib.SimpleCoreLib;
+import mod.alexndr.simplecorelib.client.ClientModEventSubscriber;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
@@ -30,8 +33,12 @@ public abstract class VeryAbstractFurnaceVariantCategory<T extends AlternateFuel
 	protected static final int fuelSlot = 1;
 	protected static final int outputSlot = 2;
 
+	protected final IDrawableStatic background;
 	protected final IDrawableStatic staticFlame;
 	protected final IDrawableAnimated animatedFlame;
+	protected final IDrawableStatic flameTransparentBackground;
+	protected Component localizedName;
+
     protected Component smeltCountText;
     protected T fuelRecipe;
     
@@ -40,8 +47,19 @@ public abstract class VeryAbstractFurnaceVariantCategory<T extends AlternateFuel
 		staticFlame = guiHelper.createDrawable(RECIPE_GUI_VANILLA, 82, 114, 14, 14);
 		animatedFlame = guiHelper.createAnimatedDrawable(staticFlame, 300, IDrawableAnimated.StartDirection.TOP, true);
 		
+        // width of the recipe depends on the text, which is different in each language
+        Minecraft minecraft = Minecraft.getInstance();
+        Font fontRenderer = minecraft.font;
+        
 		// can't call createSmeltCountText() yet because fuelRecipe does not exist.
 		this.smeltCountText = new TranslatableComponent("gui.jei.category.fuel.smeltCount",10000000);
+        int stringWidth = fontRenderer.width(smeltCountText.getString());
+
+        background = guiHelper.drawableBuilder(VeryAbstractFurnaceVariantCategory.RECIPE_GUI_VANILLA, 0, 134, 18, 34)
+            .addPadding(0, 0, 0, stringWidth + 20)
+            .build();
+
+        flameTransparentBackground = ClientModEventSubscriber.textures.getFlameIcon();
 	}
 
     public Component getSmeltCountText()
@@ -57,22 +75,19 @@ public abstract class VeryAbstractFurnaceVariantCategory<T extends AlternateFuel
     @Override
     public Component getTitle()
     {
-        // TODO Auto-generated method stub
-        return null;
+        return localizedName;
     }
 
     @Override
     public IDrawable getBackground()
     {
-        // TODO Auto-generated method stub
-        return null;
+        return background;
     }
 
     @Override
     public IDrawable getIcon()
     {
-        // TODO Auto-generated method stub
-        return null;
+        return flameTransparentBackground;
     }
 
     @Override
--- src/main/java/mod/alexndr/simplecorelib/client/ClientModEventSubscriber.java
+++ src/main/java/mod/alexndr/simplecorelib/client/ClientModEventSubscriber.java
@@ -1,19 +1,26 @@
 package mod.alexndr.simplecorelib.client;
 
 import mod.alexndr.simplecorelib.SimpleCoreLib;
+import mod.alexndr.simplecorelib.api.client.gui.SimpleSpriteUploader;
+import mod.alexndr.simplecorelib.api.client.gui.Textures;
 import mod.alexndr.simplecorelib.client.gui.TestFurnaceScreen;
 import mod.alexndr.simplecorelib.content.TestFurnaceContainerMenu;
 import mod.alexndr.simplecorelib.init.ModMenuTypes;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @EventBusSubscriber(modid = SimpleCoreLib.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public final class ClientModEventSubscriber
 {
+    public static Textures textures;
+
     /**
      * We need to register our renderers on the client because rendering code does not exist on the server
      * and trying to use it on a dedicated server will crash the game.
@@ -30,5 +37,18 @@ public final class ClientModEventSubscriber
             MenuScreens.register((MenuType<TestFurnaceContainerMenu>) ModMenuTypes.test_furnace.get(), TestFurnaceScreen::new);
         });
     }
+
+    @SubscribeEvent
+    public static void onRegisterClientReloadListenersEvent(final RegisterClientReloadListenersEvent event)
+    {
+        if (ModList.get().isLoaded("jei"))
+        {
+            // add things to texture atlas.
+            Minecraft minecraft = Minecraft.getInstance();
+            SimpleSpriteUploader spriteUploader = new SimpleSpriteUploader(minecraft.textureManager, SimpleCoreLib.SIMPLE_TEXTURE_ATLAS);
+            textures = new Textures(spriteUploader);
+            event.registerReloadListener(spriteUploader);
+        }
+    } // end onRegisterClientReloadListenersEvent
 
 } // end class
