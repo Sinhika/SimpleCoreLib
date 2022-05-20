6e70c0fbbd1a30b31f8323f8f20f1f40d35ceb54
Sinhika <sinhika@republicofnewhome.org>
2022-05-20 17:28:26 -0500

    breaking all bow animations was not the way to fix an exception. Turns out ItemPropeties.register() needs to be in a work queue in setup

--- src/main/java/mod/alexndr/simplecorelib/api/client/ClientUtils.java
+++ src/main/java/mod/alexndr/simplecorelib/api/client/ClientUtils.java
@@ -10,7 +10,6 @@ import net.minecraft.world.item.BowItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.event.FOVModifierEvent;
-import net.minecraftforge.registries.ForgeRegistries;
 
 /**
  * small functions that are used client-side.
@@ -59,20 +58,20 @@ public final class ClientUtils
      */
     public static void setupBowModelProperties(Item bow) 
     {
-        String modid = ForgeRegistries.ITEMS.getKey(bow).getNamespace();
+        // String modid = ForgeRegistries.ITEMS.getKey(bow).getNamespace();
         
-        ItemProperties.register(bow, new ResourceLocation(modid, "pull"), (p0, p1, p2, p4) -> {
+        ItemProperties.register(bow, new ResourceLocation("pull"), (p0, p1, p2, p4) -> {
             if (p2 == null)
             {
                 return 0.0F;
-            }
+            } 
             else
             {
                 return p2.getUseItem() != p0 ? 0.0F
                         : (float) (p0.getUseDuration() - p2.getUseItemRemainingTicks()) / 20.0F;
             }
         });
-        ItemProperties.register(bow, new ResourceLocation(modid, "pulling"), (p0, p1, p2, p4) -> {
+        ItemProperties.register(bow, new ResourceLocation("pulling"), (p0, p1, p2, p4) -> {
             return p2 != null && p2.isUsingItem() && p2.getUseItem() == p0 ? 1.0F : 0.0F;
         });
     } // end setupBowModelProperties()
