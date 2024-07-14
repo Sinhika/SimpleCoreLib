2f77a28bff99f842406e45d99b462414f6e4abdc
Sinhika <sinhika@republicofnewhome.org>
2024-07-14 10:17:42 -0500

    renamed old NameUtils.fromItem() to NameUtils.fromItemStack(), because it uses itemStacks, not Items as arg. Created a new NameUtils.fromItem() that actually takes Item as an arg

--- src/main/java/mod/alexndr/simplecorelib/api/helpers/NameUtils.java
+++ src/main/java/mod/alexndr/simplecorelib/api/helpers/NameUtils.java
@@ -3,6 +3,7 @@ package mod.alexndr.simplecorelib.api.helpers;
 import com.google.common.base.Preconditions;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 public final class NameUtils
@@ -15,10 +16,14 @@ public final class NameUtils
      * @return The registry name
      * @throws NullPointerException if registry name is null
      */
-    public static ResourceLocation fromItem(ItemStack stack)
+    public static ResourceLocation fromItemStack(ItemStack stack)
     {
-//        ResourceLocation name = stack.getItem().getRegistryName();
-        ResourceLocation name = BuiltInRegistries.ITEM.getKey(stack.getItem());
+        return NameUtils.fromItem(stack.getItem());
+    }
+
+    public static ResourceLocation fromItem(Item item)
+    {
+        ResourceLocation name = BuiltInRegistries.ITEM.getKey(item);
         Preconditions.checkNotNull(name, "Name is null, make sure the object has been registered correctly");
         return name;
     }
