098bf8f06fa6da3be5c88c399a905d8309704395
Sinhika <sinhika@republicofnewhome.org>
2024-10-05 12:47:30 -0500

    fix sprite textures to have transparent backgrounds

--- src/main/java/mod/alexndr/simplecorelib/api/client/gui/SomewhatAbstractFurnaceScreen.java
+++ src/main/java/mod/alexndr/simplecorelib/api/client/gui/SomewhatAbstractFurnaceScreen.java
@@ -1,5 +1,6 @@
 package mod.alexndr.simplecorelib.api.client.gui;
 
+import mod.alexndr.simplecorelib.SimpleCoreLib;
 import net.minecraft.client.gui.screens.inventory.AbstractFurnaceScreen;
 import net.minecraft.client.gui.screens.recipebook.AbstractFurnaceRecipeBookComponent;
 import net.minecraft.network.chat.Component;
@@ -12,8 +13,23 @@ import net.neoforged.api.distmarker.OnlyIn;
 @OnlyIn(Dist.CLIENT)
 public abstract class SomewhatAbstractFurnaceScreen<T extends AbstractFurnaceMenu> extends AbstractFurnaceScreen<T>
 {
-    private static final ResourceLocation LIT_PROGRESS_SPRITE = new ResourceLocation("container/furnace/lit_progress");
-    private static final ResourceLocation BURN_PROGRESS_SPRITE = new ResourceLocation("container/furnace/burn_progress");
+    // for furnaces
+    protected static final ResourceLocation LIT_PROGRESS_SPRITE =
+            new ResourceLocation(SimpleCoreLib.MODID, "container/furnace/lit_progress");
+    protected static final ResourceLocation BURN_PROGRESS_SPRITE =
+            new ResourceLocation(SimpleCoreLib.MODID, "container/furnace/burn_progress");
+
+    // for blast_furnace
+    protected static final ResourceLocation BLAST_FURNACE_LIT_PROGRESS_SPRITE =
+            new ResourceLocation(SimpleCoreLib.MODID, "container/blast_furnace/lit_progress");
+    protected static final ResourceLocation BLAST_FURNACE_BURN_PROGRESS_SPRITE =
+            new ResourceLocation(SimpleCoreLib.MODID, "container/blast_furnace/burn_progress");
+
+    // for smoker
+    protected static final ResourceLocation SMOKER_LIT_PROGRESS_SPRITE =
+            new ResourceLocation(SimpleCoreLib.MODID, "container/smoker/lit_progress");
+    protected static final ResourceLocation SMOKER_BURN_PROGRESS_SPRITE =
+            new ResourceLocation(SimpleCoreLib.MODID, "container/smoker/burn_progress");
 
     public SomewhatAbstractFurnaceScreen(T menu,
                                          AbstractFurnaceRecipeBookComponent recipeBookComponent,
@@ -21,7 +37,8 @@ public abstract class SomewhatAbstractFurnaceScreen<T extends AbstractFurnaceMen
                                          Component title,
                                          ResourceLocation texture)
     {
-        super(menu, recipeBookComponent, playerInventory, title, texture, LIT_PROGRESS_SPRITE, BURN_PROGRESS_SPRITE);
+        super(menu, recipeBookComponent, playerInventory, title, texture,
+                SomewhatAbstractFurnaceScreen.LIT_PROGRESS_SPRITE, SomewhatAbstractFurnaceScreen.BURN_PROGRESS_SPRITE);
     }
 
     public SomewhatAbstractFurnaceScreen(T menu,
